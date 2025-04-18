7f534d6b28c5d285910353820222e55eb437c42c
aernegy <jayden.chanzz06@gmail.com>
2025-04-18 19:21:30 +0800

    GUI completed. Functionality including delete, add, select, and deselect completed.

--- book.py
+++ book.py
@@ -1,3 +1,5 @@
 class Book:
-    def __init__(self):
-        pass
+    def __init__(self, title, author, genre):
+        self.title = title
+        self.author = author
+        self.genre = genre
--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from tkinter import *
-from tkinter import tkk
+from tkinter import ttk
 from tkinter import messagebox
 
 
 class Main:
     def __init__(self):
-        pass
-    
+        root = Tk()
+        root.title("Library Catalog")
+        root.geometry("500x400")
+
+        self.start_gui(root)        
+
+        root.mainloop()
+
+
+    def start_gui(self, root):
+        main_window = Frame(root)
+        main_window.pack(expand=TRUE)
+
+        search_label = Label(main_window, text="Search for a title or author: ", anchor="w")
+        search_label.grid(row=0, column=0, sticky="w")
+
+        search_value = StringVar()
+        search_entry = ttk.Entry(root, textvariable=search_value)
+        search_entry.grid(row=1, column=0, sticky="ew", padx=20)
+
+        root.columnconfigure(0, weight=1)
+
+Main()
